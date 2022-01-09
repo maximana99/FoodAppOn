1483ffa912e1dcf09298935075cd62edf3cf75ea
Ana Maria Maxim <maximana1999@yahoo.com>
2022-01-09 23:48:45 +0200

    Implemented communication between pages

--- screens/MainLayout.js
+++ screens/MainLayout.js
@@ -93,6 +93,8 @@ const TabButton = ({label, icon, isFocused, outerContainerStyle, innerContainerS
 
 const MainLayout = ({drawerAnimationStyle, navigation, selectedTab, setSelectedTab}) => {
 
+    const flatListRef = React.useRef()
+
     //Reanimated Shared Value
     const homeTabFlex = useSharedValue(1)
     const homeTabColor = useSharedValue(COLORS.white)
@@ -173,6 +175,12 @@ const MainLayout = ({drawerAnimationStyle, navigation, selectedTab, setSelectedT
 
     React.useEffect(() => {
         if (selectedTab == constants.screens.home) {
+
+            flatListRef?.current?.scrollToIndex({
+                index: 0,
+                animated: false
+            })
+
             homeTabFlex.value = withTiming(4, {duration: 500})
             homeTabColor.value = withTiming(COLORS.primary, {duration: 500})
         } else {
@@ -181,6 +189,12 @@ const MainLayout = ({drawerAnimationStyle, navigation, selectedTab, setSelectedT
         }
 
         if (selectedTab == constants.screens.search) {
+
+            flatListRef?.current?.scrollToIndex({
+                index: 1,
+                animated: false
+            })
+
             searchTabFlex.value = withTiming(4, {duration: 500})
             searchTabColor.value = withTiming(COLORS.primary, {duration: 500})
         } else {
@@ -189,6 +203,12 @@ const MainLayout = ({drawerAnimationStyle, navigation, selectedTab, setSelectedT
         }
 
         if (selectedTab == constants.screens.cart) {
+
+            flatListRef?.current?.scrollToIndex({
+                index: 2,
+                animated: false
+            })
+
             cartTabFlex.value = withTiming(4, {duration: 500})
             cartTabColor.value = withTiming(COLORS.primary, {duration: 500})
         } else {
@@ -197,6 +217,12 @@ const MainLayout = ({drawerAnimationStyle, navigation, selectedTab, setSelectedT
         }
 
         if (selectedTab == constants.screens.favourite) {
+
+            flatListRef?.current?.scrollToIndex({
+                index: 3,
+                animated: false
+            })
+
             favouriteTabFlex.value = withTiming(4, {duration: 500})
             favouriteTabColor.value = withTiming(COLORS.green, {duration: 500})
         } else {
@@ -205,6 +231,12 @@ const MainLayout = ({drawerAnimationStyle, navigation, selectedTab, setSelectedT
         }
 
         if (selectedTab == constants.screens.notification) {
+
+            flatListRef?.current?.scrollToIndex({
+                index: 4,
+                animated: false
+            })
+
             notificationTabFlex.value = withTiming(4, {duration: 500})
             notificationTabColor.value = withTiming(COLORS.primary, {duration: 500})
         } else {
@@ -279,7 +311,34 @@ const MainLayout = ({drawerAnimationStyle, navigation, selectedTab, setSelectedT
                     flex: 1
                 }}
             >
-            <Text>MainLayout</Text>
+                <FlatList 
+                    ref={flatListRef}
+                    horizontal
+                    scrollEnabled={false}
+                    pagingEnabled
+                    snapToAlignment="center"
+                    snapToInterval={SIZES.width}
+                    showsHorizontalScrollIndicator={false}
+                    data={constants.bottom_tabs}
+                    keyExtractor={item => `${item.id}`}
+                    renderItem={({ item, index}) => {
+                        return (
+                            <View
+                                style={{
+                                    height: SIZES.height,
+                                    width: SIZES.width
+                                }}
+                            >
+                               {item.label == constants.screens.home && <Home />}
+                               {item.label == constants.screens.search && <Search />}
+                               {item.label == constants.screens.cart && <CartTab />}
+                               {item.label == constants.screens.favourite && <Favourite />}
+                               {item.label == constants.screens.notification && <Notification />}
+
+                            </View>
+                        )
+                    }}
+                />
             </View>
 
 
