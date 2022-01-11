3ef488376b08b75c13907a1fb7237c3e7a009bdc
Ana Maria Maxim <maximana1999@yahoo.com>
2022-01-12 00:41:44 +0200

    Added from input for email for sign in

--- screens/Sign/SignIn.js
+++ screens/Sign/SignIn.js
@@ -11,6 +11,9 @@ import {
 import { AuthLayout } from '../';
 //{navigation}
 const SignIn = () => {
+    const [email, setEmail] = React.useState("")
+    const [password, setPassword] = React.useState("")
+    const [emailError, setEmailError] = React.useState("")
     return (
         // <View style={{
         //         marginTop: 100
@@ -34,6 +37,43 @@ const SignIn = () => {
             <AuthLayout
             title="Let's Sign You In"
             subtitle="Welcome back!">
+
+            <View 
+                style={{
+                    flex: 1,
+                    marginTop: SIZES.padding * 2
+                }}
+            >
+                {/*Form inputs*/}
+                <FormInput
+                    label="Email"
+                    keyboardType="email-address"
+                    autoCompleteType="email"
+                    onChange={(value) => {
+                        // validate email
+                        setEmail(value)
+                    }}
+                    errorMsg={emailError}
+                    appendComponent={
+                        <View
+                            style={{
+                                justifyContent:'center'
+                            }}
+                        >
+                            <Image
+                                source={icons.correct}
+                                style={{
+                                    height:20,
+                                    width: 20,
+                                    tintColor: COLORS.green
+                                }}
+                            />
+
+                        </View>
+                    }
+                />
+
+            </View>
                
             </AuthLayout>
     )
